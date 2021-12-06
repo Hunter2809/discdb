0dc2dd107719ac54c716ad208c310c3e81a6e2d7
Hunter <guptaakshat05@gmail.com>
2021-12-06 23:03:15 +0530

    Fixed init

--- discdb/__init__.py
+++ discdb/__init__.py
@@ -3,7 +3,7 @@ from .discdb import DiscDB
 __version__ = "0.1.0"
 __author__ = "Hunter"
 __copyright__ = f"Copyright 2021 - Present {__author__}"
-__title = "RSAP"
+__title__ = "DiscDB"
 __summary__ = "discdb is a fun module to save data in discord messages in the form of JSON 😁"
-__status__ = "Development"
+__status__ = "Production Ready"
 __all__ = ["DiscDB"]
